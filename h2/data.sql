3a7a18591ae8b308b8831d468c5270b2f8f1711d
skn33 <50368562+skn33@users.noreply.github.com>
2023-05-03 11:37:08 +0200

    Update data.sql

--- h2/data.sql
+++ h2/data.sql
@@ -3,7 +3,7 @@ INSERT INTO vets VALUES (default, 'Helen', 'Leary');
 INSERT INTO vets VALUES (default, 'Linda', 'Douglas');
 INSERT INTO vets VALUES (default, 'Rafael', 'Ortega');
 INSERT INTO vets VALUES (default, 'Henry', 'Stevens');
-INSERT INTO vets VALUES (default, 'Sharon', 'Jenkins');
+INSERT INTO vets VALUES (default, 'Sharonon', 'Jenkins');
 
 INSERT INTO specialties VALUES (default, 'radiology');
 INSERT INTO specialties VALUES (default, 'surgery');
